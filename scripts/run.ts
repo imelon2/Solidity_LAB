7785ddb69e643478e6d0c4c07a321a22db364a4b
imelon2 <imelon279@naver.com>
2023-09-19 18:29:57 +0900

    re

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,31 +1,33 @@
 import { ethers } from "ethers";
-import {abi} from "../artifacts/contracts/ERC20/ERC20.sol/MyToken.json";
+import {abi,bytecode} from "../artifacts/contracts/ERC20/ERC20.sol/MyToken.json";
 
 const GOERLI_RPC = "https://rpc.ankr.com/eth_goerli";
-const ADDRESS = "0xBEF286d726a9B9f12091A8B3D6a4d6Ed33825144"
-const PK = ""
-const user1 = "0x9E7c0F4B7C2299298159B4Fd3dF186A709938b1E"
+const MUMBAI_RPC = "https://rpc.ankr.com/polygon_mumbai";
+const PK = "0x972edabc1b1da76e68b2979f31e7fccb66a42fcc21abf18df6a361b2403c818e"
 
 async function main() {
-    const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC);
+    const provider = new ethers.providers.JsonRpcProvider(MUMBAI_RPC);
     const user = new ethers.Wallet(PK,provider);
 
-    const ERC20 = new ethers.Contract(ADDRESS,abi,user);
+    const ERC20 = new ethers.ContractFactory(abi,bytecode,user);
     // const balance = await ERC20.balanceOf(user.address);
     // console.log(balance);
-    console.log("RUN ERC20 APPROVE");
+    // console.log("RUN ERC20 APPROVE");
     let startTime = performance.now()
-    // const balance = await ERC20.approve(user1,"1000000000000000000",{gasLimit:21800,gasPrice:100000000}); // 1gwei
-    const balance = await user.sendTransaction({
-        from:user.address,
-        to:user.address,
-        value:"10000000000000000000",
-        gasLimit:21800,
-    })
-    console.log(balance);
+    let _ERC20 = await ERC20.deploy();
+    console.log(_ERC20.address);
     
-    const result = await balance.wait()
-    console.log(result);
+    // const balance = await ERC20.approve(user1,"1000000000000000000",{gasLimit:21800,gasPrice:100000000}); // 1gwei
+    // const balance = await user.sendTransaction({
+    //     from:user.address,
+    //     to:user.address,
+    //     value:"10000000000000000000",
+    //     gasLimit:21800,
+    // })
+    // console.log(balance);
+    
+    // const result = await balance.wait()
+    // console.log(result);
 
     let endTime = performance.now()
     console.log(endTime - startTime);
