d1eaa60067a9bce3fe10f35511360d819d4ee13c
imelon2 <imelon279@naver.com>
2023-12-06 18:12:18 +0900

    bytes32 mload

---
+++ contracts/Assembly/mload.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract mloadBytes {
+    function asem_add(bytes memory bytescode,bytes32 a) pure public returns(bytes32) {
+        bytes32 result;
+        assembly {
+            result := add(bytescode, a)
+        }
+
+        return result;
+    }
+
+    function asem_add1(bytes32 a,bytes32 b) pure public returns(bytes32) {
+        bytes32 result;
+        assembly {
+            result := add(a, b)
+        }
+
+        return result;
+    }
+
+    function asem_add2(bytes memory a,bytes memory b) pure public returns(bytes32) {
+        bytes32 result;
+        assembly {
+            result := add(a, b)
+        }
+
+        return result;
+    }
+
+    function asem_mload(bytes memory a) pure public returns(bytes32) {
+        bytes32 result;
+        assembly {
+            result := mload(a)
+        }
+
+        return result;
+    }
+
+    function asem_mload1(bytes32 a) pure public returns(bytes32) {
+        bytes32 result;
+        assembly {
+            result := mload(a)
+        }
+
+        return result;
+    }
+
+    function bytesLength1(bytes memory a) pure public returns(uint256) {
+        return a.length;
+    }
+
+    function bytesLength2(bytes32 a) pure public returns(uint256) {
+        return a.length;
+    }
+
+    function mload(bytes memory a) pure public returns(bytes memory) {
+        return a;
+    }
+}
